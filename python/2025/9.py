648a498eb4c2f28816591b48cb7d5e97a16303ea
Andrea Ambu <andreambu@gmail.com>
2025-12-09 22:31:16 +0000

    cons

--- python/2025/9.py
+++ python/2025/9.py
@@ -6,11 +6,11 @@ def parse(data):
 
 
 def part1(tiles):
-    best = 0
-    for i, (xi, yi) in enumerate(tiles):
-        for xj, yj in tiles[i + 1 :]:
-            best = max(best, (abs(xi - xj) + 1) * (abs(yi - yj) + 1))
-    return best
+    return max(
+        (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+        for i, (x1, y1) in enumerate(tiles)
+        for x2, y2 in tiles[i + 1 :]
+    )
 
 
 def part2(tiles):
@@ -21,26 +21,18 @@ def part2(tiles):
         sorted_edges.append((min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)))
     sorted_edges.sort(key=lambda x: (x[2] - x[0]) + (x[3] - x[1]), reverse=True)
 
-    def intersects(min_x, min_y, max_x, max_y):
-        for e_min_x, e_min_y, e_max_x, e_max_y in sorted_edges:
-            if (
-                min_x < e_max_x
-                and max_x > e_min_x
-                and min_y < e_max_y
-                and max_y > e_min_y
-            ):
+    def intersects(bx1, by1, bx2, by2):
+        for ex1, ey1, ex2, ey2 in sorted_edges:
+            if bx1 < ex2 and bx2 > ex1 and by1 < ey2 and by2 > ey1:
                 return True
         return False
 
     best = 0
-    for i, (xi, yi) in enumerate(tiles):
-        for xj, yj in tiles[i + 1 :]:
-            min_x, max_x = min(xi, xj), max(xi, xj)
-            min_y, max_y = min(yi, yj), max(yi, yj)
-
-            area = (max_x - min_x + 1) * (max_y - min_y + 1)
-
-            if area > best and not intersects(min_x, min_y, max_x, max_y):
+    for i, (x1, y1) in enumerate(tiles):
+        for x2, y2 in tiles[i + 1 :]:
+            bx1, by1, bx2, by2 = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
+            area = (bx2 - bx1 + 1) * (by2 - by1 + 1)
+            if area > best and not intersects(bx1, by1, bx2, by2):
                 best = area
     return best
 
